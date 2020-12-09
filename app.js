8fe64fc6c5e5d148ca3144c8dc77cb15dc615508
Amran Ahmed <amranahmedd@gmail.com>
2020-12-09 15:01:30 -0600

    refactored code so function is standalone, added comments

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const button = document.querySelector('button');
+//selecting button
+const button = document.querySelector("button");
+//selecting h1
+const h1 = document.querySelector("h1");
 
-button.addEventListener('click', function() {
-    document.body.style.backgroundColor = 'olive';
+//randomColor function 
+const randomColor = () => {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-})
+    const newColor = `rgb(${r}, ${g}, ${b})`;
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+};
+//adding eventlistener so that color is randomized whenever button is clicked by user 
+button.addEventListener("click", randomColor);
