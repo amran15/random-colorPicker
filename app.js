6742ab988199b5cced87b25d5f08f6e182c16666
Amran Ahmed <amranahmedd@gmail.com>
2020-12-09 15:16:05 -0600

    added more styling

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const button = document.querySelector("button");
 //selecting h1
 const h1 = document.querySelector("h1");
 
-//randomColor function 
-const randomColor = () => {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    const newColor = `rgb(${r}, ${g}, ${b})`;
+//adding eventlistener so that color is randomized whenever button is clicked by user 
+button.addEventListener("click", function(){
+    const newColor = randomColor();   
     document.body.style.backgroundColor = newColor;
     h1.innerText = newColor;
-};
-//adding eventlistener so that color is randomized whenever button is clicked by user 
-button.addEventListener("click", randomColor);
+});
+
+//randomColor function 
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    if (r < 150 && g < 150 && b < 150 ){
+        h1.style.color = 'white';
+    }
+    return  `rgb(${r}, ${g}, ${b})`;
+}
