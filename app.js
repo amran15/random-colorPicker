bd7b7013d2f04fa2e0b2f4ac573e958c7dccb1e7
Amran Ahmed <amranahmedd@gmail.com>
2020-12-09 15:20:54 -0600

    refactored code and added styling using vanilla js

--- app.js
+++ app.js
@@ -15,8 +15,11 @@ const randomColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    if (r < 150 && g < 150 && b < 150 ){
+    const sum = r + g + b;
+    if (sum < 300 ){
         h1.style.color = 'white';
+    } else {
+        h1.style.color = 'black';
     }
     return  `rgb(${r}, ${g}, ${b})`;
 }
